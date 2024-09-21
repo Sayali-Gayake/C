b953dcb600a4eef5e4e5484638a3bfac6891d83a
Sayali-Gayake <gayakesayali@gmail.com>
2024-09-21 11:53:56 +0530

    Add files via upload

---
+++ Looping_Statements/ex2.c
@@ -0,0 +1,99 @@
+//2. Write a C program to pront all natural numbers in reverse(From n to 1).
+//---Using while loop
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("Enter the number n");
+	scanf("%d\n",&n);
+	while(n>=1)
+	{
+		printf("%d\n",n);
+		n--;
+	}
+	printf("\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
